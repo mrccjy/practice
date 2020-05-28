3370679c638f0968f7f795a957f7c30d87379c5e
chen.junyong <236261092@qq.com>
2020-05-28 23:50:43 +0800

    块交换算法

--- src/main/java/com/practice/program/pearl/BlockSwitch.java
+++ src/main/java/com/practice/program/pearl/BlockSwitch.java
@@ -1,17 +1,45 @@
 package com.practice.program.pearl;
 
+/**
+ * 块交换算法
+ */
 public class BlockSwitch {
     static  String originalStr;
     static char[] charStr;
 
     public static void main(String[] args) {
-        charStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-        System.out.println(swap(0, 12, 10));
+        originalStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        blockSwitch(2);
     }
 
     public static String blockSwitch(int rotdist) {
+        charStr = originalStr.toCharArray();
+        int n = charStr.length;
+        int i = rotdist,
+            p = rotdist,
+            j = n - p;
+        while (i != j) {
+            if (i > j) {
+                swap(p - i, p, j);
+                i -= j;
+            } else {
+                swap(p - i, p + j - i, i);
+                j -= i;
+            }
+            print();
+        }
+        swap(p - i, p, i);
 
-        return null;
+        return print();
+    }
+
+    public static String print() {
+        StringBuilder sb = new StringBuilder();
+        for (char c : charStr) {
+            sb.append(c);
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
     }
 
     /**
@@ -21,16 +49,13 @@ public class BlockSwitch {
      * @param size  移动个数
      * @return
      */
-    public static String swap(int start, int end, int size) {
+    public static void swap(int start, int end, int size) {
         for (int t=0, i=start, j=end; i<j && t<size; t++, i++, j++) {
             char tmp = charStr[i];
             charStr[i] = charStr[j];
             charStr[j] = tmp;
         }
-        StringBuilder sb = new StringBuilder();
-        for (char c : charStr) {
-            sb.append(c);
-        }
-        return sb.toString();
+
+        return;
     }
 }
