a8e954f0ce340205597eab2c6b202bdda4bd61b8
chen.junyong <236261092@qq.com>
2021-04-20 22:36:21 +0800

    合并2个有序链表

--- src/main/java/com/practice/leetcode/editor/cn/MergeTwoSortedLists.java
+++ src/main/java/com/practice/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -42,16 +42,13 @@ public class MergeTwoSortedLists{
        Solution solution = new MergeTwoSortedLists().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode head = null;
