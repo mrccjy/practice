878851b7d690135f931f05102f1d4b27aedaf8d3
chen.junyong <236261092@qq.com>
2021-04-21 22:07:21 +0800

    相交链表

--- src/main/java/com/practice/leetcode/editor/cn/IntersectionOfTwoLinkedLists.java
+++ src/main/java/com/practice/leetcode/editor/cn/IntersectionOfTwoLinkedLists.java
@@ -63,23 +63,23 @@ package com.practice.leetcode.editor.cn;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class IntersectionOfTwoLinkedLists{
   public static void main(String[] args) {
        Solution solution = new IntersectionOfTwoLinkedLists().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode aCurr = headA;
