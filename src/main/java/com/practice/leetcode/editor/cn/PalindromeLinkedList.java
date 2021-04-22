5e2eef306e749c50402957f6f880a2658f55db74
chenjunyong <chenjunyong@yidayuntu.cn>
2021-04-22 14:44:49 +0800

    回文连表：双指针

--- src/main/java/com/practice/leetcode/editor/cn/PalindromeLinkedList.java
+++ src/main/java/com/practice/leetcode/editor/cn/PalindromeLinkedList.java
@@ -25,16 +25,14 @@ public class PalindromeLinkedList{
        Solution solution = new PalindromeLinkedList().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
         if (head == null) {
