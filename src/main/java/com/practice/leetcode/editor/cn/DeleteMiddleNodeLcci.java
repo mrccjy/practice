c49c23d2b9717f4f09a50473c8dd35463696b0c3
chenjunyong <chenjunyong@yidayuntu.cn>
2021-04-22 16:53:23 +0800

    只访问该节点情况删除该节点

---
+++ src/main/java/com/practice/leetcode/editor/cn/DeleteMiddleNodeLcci.java
@@ -0,0 +1,35 @@
+package com.practice.leetcode.editor.cn;
+  
+//实现一种算法，删除单向链表中间的某个节点（即不是第一个或最后一个节点），假定你只能访问该节点。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：单向链表a->b->c->d->e->f中的节点c
+//结果：不返回任何数据，但该链表变为a->b->d->e->f
+// 
+// Related Topics 链表
+
+public class DeleteMiddleNodeLcci{
+  public static void main(String[] args) {
+       Solution solution = new DeleteMiddleNodeLcci().new Solution();
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
