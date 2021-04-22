2a42fa5ba976d9284cb977d0c033120b9d323c28
chenjunyong <chenjunyong@yidayuntu.cn>
2021-04-22 14:00:16 +0800

    翻转连表

---
+++ src/main/java/com/practice/leetcode/editor/cn/ReverseLinkedList.java
@@ -0,0 +1,72 @@
+package com.practice.leetcode.editor.cn;
+  
+//反转一个单链表。 
+//
+// 示例: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL 
+//
+// 进阶: 
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？ 
+// Related Topics 链表
+
+public class ReverseLinkedList{
+  public static void main(String[] args) {
+       Solution solution = new ReverseLinkedList().new Solution();
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return interateListNode(head);
+    }
+
+    /**
+     * 递归方式实现
+     * @param head
+     * @return
+     */
+    private ListNode interateListNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+        }
+        return prev;
+    }
+
+    /**
+     * 递归方式实现
+     * @param head
+     * @return
+     */
+    private ListNode reverListNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
