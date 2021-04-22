7979b3aa6b13f745997eb9eaaeb2186a9acd446d
chenjunyong <chenjunyong@yidayuntu.cn>
2021-04-22 14:59:53 +0800

    删除链表中的节点

--- src/main/java/com/practice/leetcode/editor/cn/DeleteNodeInALinkedList.java
+++ src/main/java/com/practice/leetcode/editor/cn/DeleteNodeInALinkedList.java
@@ -41,14 +41,11 @@ public class DeleteNodeInALinkedList{
        Solution solution = new DeleteNodeInALinkedList().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
 class Solution {
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
