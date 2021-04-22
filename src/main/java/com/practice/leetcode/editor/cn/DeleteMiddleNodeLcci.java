8596b54e1c576c6e3ab968d15eb73a8d9f43bade
chenjunyong <chenjunyong@yidayuntu.cn>
2021-04-22 16:53:35 +0800

    只访问该节点情况删除该节点

--- src/main/java/com/practice/leetcode/editor/cn/DeleteMiddleNodeLcci.java
+++ src/main/java/com/practice/leetcode/editor/cn/DeleteMiddleNodeLcci.java
@@ -16,14 +16,11 @@ public class DeleteMiddleNodeLcci{
        Solution solution = new DeleteMiddleNodeLcci().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
 class Solution {
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
