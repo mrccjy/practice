a9643308d39122e9624be2b6b54802b7fb3b3b49
chenjunyong <chenjunyong@yidayuntu.cn>
2021-04-22 14:00:36 +0800

    翻转连表

--- src/main/java/com/practice/leetcode/editor/cn/ReverseLinkedList.java
+++ src/main/java/com/practice/leetcode/editor/cn/ReverseLinkedList.java
@@ -16,16 +16,13 @@ public class ReverseLinkedList{
        Solution solution = new ReverseLinkedList().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 class Solution {
     public ListNode reverseList(ListNode head) {
         return interateListNode(head);
