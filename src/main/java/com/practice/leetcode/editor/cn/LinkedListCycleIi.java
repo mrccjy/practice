19d3e37fe5d710c35ffdd03a8c54f5104fc83383
chen.junyong <236261092@qq.com>
2021-04-23 19:57:44 +0800

    环形链表，找出入环口：快慢指针

--- src/main/java/com/practice/leetcode/editor/cn/LinkedListCycleIi.java
+++ src/main/java/com/practice/leetcode/editor/cn/LinkedListCycleIi.java
@@ -62,17 +62,14 @@ public class LinkedListCycleIi{
        Solution solution = new LinkedListCycleIi().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode slow = head;
