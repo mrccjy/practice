0fa69d4a57da303202859144550e2b86b220fe59
chen.junyong <236261092@qq.com>
2021-04-23 21:39:26 +0800

    重排链表

--- src/main/java/com/practice/leetcode/editor/cn/ReorderList.java
+++ src/main/java/com/practice/leetcode/editor/cn/ReorderList.java
@@ -20,16 +20,13 @@ public class ReorderList{
        Solution solution = new ReorderList().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 class Solution {
     public void reorderList(ListNode head) {
         if (head == null || head.next == null) {
