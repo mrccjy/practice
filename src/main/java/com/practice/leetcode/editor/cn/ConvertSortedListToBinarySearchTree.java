c4210ae6a291b4044dd009d5a41d943f9c2f7725
chenjunyong <chenjunyong@yidayuntu.cn>
2021-04-23 15:57:46 +0800

    修改注释

--- src/main/java/com/practice/leetcode/editor/cn/ConvertSortedListToBinarySearchTree.java
+++ src/main/java/com/practice/leetcode/editor/cn/ConvertSortedListToBinarySearchTree.java
@@ -23,31 +23,27 @@ public class ConvertSortedListToBinarySearchTree{
        Solution solution = new ConvertSortedListToBinarySearchTree().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+ public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
         if (head == null) {
