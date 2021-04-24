c2e143ed3fe8b2b20c08aa9d26cfad7ff2fffdcf
chen.junyong <236261092@qq.com>
2021-04-24 12:58:49 +0800

    排序链表：递归

--- src/main/java/com/practice/leetcode/editor/cn/SortList.java
+++ src/main/java/com/practice/leetcode/editor/cn/SortList.java
@@ -53,16 +53,13 @@ public class SortList{
        solution.sortList(head);
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
 class Solution {
     public ListNode sortList(ListNode head) {
         return recursiveSort(head, null);
