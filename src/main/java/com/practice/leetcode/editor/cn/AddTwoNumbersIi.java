f00218fd5915e6199189daff6dcc29412f64e282
chen.junyong <236261092@qq.com>
2021-04-24 16:53:26 +0800

    两数相加II

--- src/main/java/com/practice/leetcode/editor/cn/AddTwoNumbersIi.java
+++ src/main/java/com/practice/leetcode/editor/cn/AddTwoNumbersIi.java
@@ -27,16 +27,13 @@ public class AddTwoNumbersIi{
        Solution solution = new AddTwoNumbersIi().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         Stack<Integer> stack1 = new Stack<>();
