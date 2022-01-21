e4307aa3dcd6e4f4d9dd4415580b53697d9a51d7
chenjunyong <chenjunyong@yidayuntu.cn>
2022-01-21 18:29:46 +0800

    change

--- src/main/java/com/practice/leetcode/editor/cn/MinimumDepthOfBinaryTree.java
+++ src/main/java/com/practice/leetcode/editor/cn/MinimumDepthOfBinaryTree.java
@@ -40,21 +40,19 @@ public class MinimumDepthOfBinaryTree{
        Solution solution = new MinimumDepthOfBinaryTree().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 class Solution {
     public int minDepth(TreeNode root) {
         if (root == null) {
