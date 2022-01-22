4cf96cda2c7c03ca25cd5a65def38d8b0e9d511b
chenjunyong <chenjunyong@yidayuntu.cn>
2022-01-22 13:23:03 +0800

    队列迭代-二叉树：相同的树

--- src/main/java/com/practice/leetcode/editor/cn/SameTree.java
+++ src/main/java/com/practice/leetcode/editor/cn/SameTree.java
@@ -60,6 +60,19 @@ public class SameTree{
  *     }
  * }
  */
+
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
