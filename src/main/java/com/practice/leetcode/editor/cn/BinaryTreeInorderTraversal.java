9d5661582529d852a1a8fd288746efb2dc6230ce
chenjunyong <chenjunyong@yidayuntu.cn>
2022-01-22 12:24:43 +0800

    迭代：二叉树中序遍历

---
+++ src/main/java/com/practice/leetcode/editor/cn/BinaryTreeInorderTraversal.java
@@ -0,0 +1,110 @@
+package com.practice.leetcode.editor.cn;
+  
+//给定一个二叉树的根节点 root ，返回它的 中序 遍历。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1]
+//输出：[1]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：root = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：root = [1,null,2]
+//输出：[1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 深度优先搜索 二叉树
+
+import java.util.*;
+
+public class BinaryTreeInorderTraversal{
+  public static void main(String[] args) {
+       Solution solution = new BinaryTreeInorderTraversal().new Solution();
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }*/
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> list = new ArrayList<>();
+        TreeNode n = root;
+        while (n != null || !stack.isEmpty()) {
+            while (n != null) {
+                stack.push(n);
+                n = n.left;
+            }
+            n = stack.pop();
+            list.add(n.val);
+            n = n.right;
+        }
+        return list;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
