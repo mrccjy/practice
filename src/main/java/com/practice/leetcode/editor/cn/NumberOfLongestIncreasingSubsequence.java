5618eb1d76112d8ec8f2a6ca51278399df039e98
chenjunyong <chenjunyong@yidayuntu.cn>
2022-03-23 12:14:47 +0800

    动态规划：最长递增子序列

---
+++ src/main/java/com/practice/leetcode/editor/cn/NumberOfLongestIncreasingSubsequence.java
@@ -0,0 +1,49 @@
+package com.practice.leetcode.editor.cn;
+  
+//给定一个未排序的整数数组 nums ， 返回最长递增子序列的个数 。 
+//
+// 注意 这个数列必须是 严格 递增的。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: [1,3,5,4,7]
+//输出: 2
+//解释: 有两个最长递增子序列，分别是 [1, 3, 4, 7] 和[1, 3, 5, 7]。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [2,2,2,2,2]
+//输出: 5
+//解释: 最长递增子序列的长度是1，并且存在5个子序列的长度为1，因此输出5。
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+//
+// 
+// 1 <= nums.length <= 2000 
+// -106 <= nums[i] <= 106 
+// 
+// Related Topics 树状数组 线段树 数组 动态规划
+
+public class NumberOfLongestIncreasingSubsequence{
+  public static void main(String[] args) {
+       Solution solution = new NumberOfLongestIncreasingSubsequence().new Solution();
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
