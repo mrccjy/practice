43331ca477e12c0b2452fd637041fbfa17bbfbf7
chenjunyong <chenjunyong@yidayuntu.cn>
2022-04-08 09:38:34 +0800

    修改注释

--- src/main/java/com/practice/leetcode/editor/cn/SubtreeOfAnotherTree.java
+++ src/main/java/com/practice/leetcode/editor/cn/SubtreeOfAnotherTree.java
@@ -45,21 +45,18 @@ public class SubtreeOfAnotherTree{
        Solution solution = new SubtreeOfAnotherTree().new Solution();
   }
   //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if (root == null && subRoot == null) {
