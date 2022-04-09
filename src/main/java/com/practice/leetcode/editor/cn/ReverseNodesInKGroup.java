a15b3e004e93686c3af074001ac0f3603791ff6b
chen.junyong <236261092@qq.com>
2022-04-09 11:34:52 +0800

    链表：k个一组返转链表

--- src/main/java/com/practice/leetcode/editor/cn/ReverseNodesInKGroup.java
+++ src/main/java/com/practice/leetcode/editor/cn/ReverseNodesInKGroup.java
@@ -68,13 +68,13 @@ public class ReverseNodesInKGroup{
        solution.reverseKGroup(head, 2);
   }
   //leetcode submit region begin(Prohibit modification and deletion)
- /*  static class ListNode {
+   static class ListNode {
       int val;
       ListNode next;
       ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }*/
+  }
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         if (head == null || k ==1) {
